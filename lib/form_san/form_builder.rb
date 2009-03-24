312e6826deed7df3eca937ca28db9553aa87f798
Manfred Stienstra <manfred@fngtps.com>
2009-03-24 12:22:50 +0100

    Revert "Add a fields and fieldset method to the form builder."
    This reverts commit 8284175ebb0cce7feecb54ea07350a8e06a379a7.

--- lib/form_san/form_builder.rb
+++ lib/form_san/form_builder.rb
@@ -1,28 +1,12 @@
 module FormSan
   class FormBuilder
     attr_accessor :output
-    alias to_s output
+    alias output to_s
     
     def initialize(record, options={})
       @output = ''
       @record = record
       @options = options
-    end
-    
-    def fieldset(html_options={}, &block)
-      html_options[:class] ||= []
-      html_options[:class] << 'fieldset'
-      output << FormSan.content_tag('div', html_options) do
-        block.call(self) if block_given?
-      end
-    end
-    
-    def fields(html_options={}, &block)
-      html_options[:class] ||= []
-      html_options[:class] << 'fields'
-      output << FormSan.content_tag('div', html_options) do
-        block.call(self) if block_given?
-      end
     end
   end
 end
