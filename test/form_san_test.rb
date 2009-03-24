fcd630673dc5034fb3809afcbfa06c0c24ea4dcb
Manfred Stienstra <manfred@fngtps.com>
2009-03-24 09:43:33 +0100

    Add tests for hash_to_attributes.

--- test/form_san_test.rb
+++ test/form_san_test.rb
@@ -1,6 +1,14 @@
 require File.expand_path('../helper', __FILE__)
 
 class FormSanTest < ActiveSupport::TestCase
+  test "converts a hash to HTML tag attributes" do
+    assert_equal '', FormSan.hash_to_attributes({})
+    assert_equal ' id="mine"', FormSan.hash_to_attributes({:id => 'mine'})
+    assert_equal ' style="width: 10px;" id="mine"', FormSan.hash_to_attributes({:id => 'mine', :style => 'width: 10px;'})
+    assert_equal ' class="current active"', FormSan.hash_to_attributes({:class => ['current', 'active']})
+    assert_equal ' class="current active"', FormSan.hash_to_attributes({'class' => [:current, :active]})
+  end
+  
   test "constructs a HTML tag with content" do
     assert_equal '<div></div>', FormSan.content_tag('div')
     assert_equal '<div></div>', FormSan.content_tag(:div)
