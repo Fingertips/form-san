4dfe147581310929c75a471849d133dd76ce4ccf
Manfred Stienstra <manfred@fngtps.com>
2009-03-24 15:21:20 +0100

    Add a label tag helper. Simplify the fieldset and fields helper, and add a field helper.

--- lib/form_san/form_builder.rb
+++ lib/form_san/form_builder.rb
@@ -8,19 +8,32 @@ module FormSan
       @options = options
     end
     
-    def fields(html_options={}, &block)
-      html_options[:class] ||= []
-      html_options[:class] << 'fields'
-      FormSan.content_tag(output_buffer, 'div', html_options) do
-        block.call(self) if block_given?
-      end
+    def fieldset(&block)
+      FormSan.content_tag(output_buffer, 'div', :class => 'fieldset') { block.call(self) }
     end
     
-    def fieldset(html_options={}, &block)
-      html_options[:class] ||= []
-      html_options[:class] << 'fieldset'
-      FormSan.content_tag(output_buffer, 'div', html_options) do
-        block.call(self) if block_given?
+    def fields(&block)
+      FormSan.content_tag(output_buffer, 'div', :class => 'fields') { block.call(self) }
+    end
+    
+    def field(&block)
+      FormSan.content_tag(output_buffer, 'div', :class => 'field') { block.call(self) }
+    end
+    
+    def label(attribute, humanized_attribute=nil, html_options={}, &block)
+      humanized_attribute ||= attribute.to_s.humanize
+      if block_given?
+        FormSan.content_tag(output_buffer, 'label', html_options) do
+          block.call(self)
+          output_buffer << " #{humanized_attribute}"
+        end
+      else
+        html_options[:for] ||= "#{@record.class.name.downcase}_#{attribute}"
+        FormSan.content_tag(output_buffer, 'div', :class => 'label') do
+          FormSan.content_tag(output_buffer, 'label', html_options) do
+            output_buffer << humanized_attribute
+          end
+        end
       end
     end
   end
--- test/form_builder_test.rb
+++ test/form_builder_test.rb
@@ -8,23 +8,36 @@ class FormBuilderTest < ActiveSupport::TestCase
     @builder = FormSan::FormBuilder.new(@output_buffer, Book.new)
   end
   
-  test "construct a fieldset" do
-    @builder.fieldset
-    assert_equal '<div class="fieldset"></div>', @output_buffer
-  end
-  
   test "construct a fieldset with contents" do
     @builder.fieldset { @output_buffer << 'content' }
     assert_equal '<div class="fieldset">content</div>', @output_buffer
   end
   
-  test "construct a group of fields" do
-    @builder.fields
-    assert_equal '<div class="fields"></div>', @output_buffer
-  end
-  
   test "construct a group of fields with content" do
     @builder.fields { @output_buffer << 'content' }
     assert_equal '<div class="fields">content</div>', @output_buffer
+  end
+  
+  test "construct a field with content" do
+    @builder.field { @output_buffer << 'content' }
+    assert_equal '<div class="field">content</div>', @output_buffer
+  end
+  
+  
+  test "construct a default label" do
+    @builder.label :published
+    assert_equal '<div class="label"><label for="book_published">Published</label></div>', @output_buffer
+  end
+  
+  test "construct a label with humanized attribute" do
+    @builder.label :published, 'Visible online'
+    assert_equal '<div class="label"><label for="book_published">Visible online</label></div>', @output_buffer
+  end
+  
+  test "construct a custom label with contents" do
+    @builder.label(:published, 'Published') do
+      @output_buffer << '<input type="checkbox" name="make" value="money" />'
+    end
+    assert_equal '<label><input type="checkbox" name="make" value="money" /> Published</label>', @output_buffer
   end
 end
